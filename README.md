9f4aacee70b5c9c8489bd95633e5b90b4821627c
AdvTech <none>
2024-01-24 19:52:45 +0800

    Upload Code.

---
+++ README.md
@@ -0,0 +1,46 @@
+## Getting Started
+
+
+### Installation
+
+```bash
+conda create -n adaptunimatch python=3.10.4
+conda activate adaptunimatch
+pip install -r requirements.txt
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 -f https://download.pytorch.org/whl/torch_stable.html
+```
+
+### Pretrained Backbone
+
+[ResNet-50](https://drive.google.com/file/d/1mqUrqFvTQ0k5QEotk4oiOFyP6B9dVZXS/view?usp=sharing) | [ResNet-101](https://drive.google.com/file/d/1Rx0legsMolCWENpfvE2jUScT3ogalMO8/view?usp=sharing) | [Xception-65](https://drive.google.com/open?id=1_j_mE07tiV24xXOJw4XDze0-a0NAhNVi)
+
+```
+├── ./pretrained
+    ├── resnet50.pth
+    ├── resnet101.pth
+    └── xception.pth
+```
+
+### Dataset
+
+- Pascal: [JPEGImages](http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar) | [SegmentationClass](https://drive.google.com/file/d/1ikrDlsai5QSf2GiSUR3f8PZUzyTubcuF/view?usp=sharing)
+
+Please modify your dataset path in configuration files.
+
+## Usage
+
+### AdaptUniMatch
+
+```bash
+# use torch.distributed.launch
+sh scripts/train.sh <num_gpu> <port>
+# to fully reproduce our results, the <num_gpu> should be set as 4 on all three datasets
+# otherwise, you need to adjust the learning rate accordingly
+
+# or use slurm
+# sh scripts/slurm_train.sh <num_gpu> <port> <partition>
+```
+
+To train on other datasets or splits, please modify
+``dataset`` and ``split`` in [train.sh](https://github.com/LiheYoung/UniMatch/blob/main/scripts/train.sh).
+
---
+++ new_method/consistency.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+
+def ce_loss(logits, targets, reduction='none'):
+    """
+    cross entropy loss in pytorch.
+
+    Args:
+        logits: logit values, shape=[Batch size, # of classes]
+        targets: integer or vector, shape=[Batch size] or [Batch size, # of classes]
+        # use_hard_labels: If True, targets have [Batch size] shape with int values. If False, the target is vector (default True)
+        reduction: the reduction argument
+    """
+    if logits.shape == targets.shape:
+        # one-hot target
+        log_pred = F.log_softmax(logits, dim=-1)
+        nll_loss = torch.sum(-targets * log_pred, dim=1)
+        if reduction == 'none':
+            return nll_loss
+        else:
+            return nll_loss.mean()
+    else:
+        log_pred = F.log_softmax(logits, dim=-1)
+        return F.nll_loss(log_pred, targets, reduction=reduction)
+
+
+
+def consistency_loss(logits, targets, name='ce', mask=None):
+    """
+    consistency regularization loss in semi-supervised learning.
+
+    Args:
+        logits: logit to calculate the loss on and back-propagation, usually being the strong-augmented unlabeled samples
+        targets: pseudo-labels (either hard label or soft label)
+        name: use cross-entropy ('ce') or mean-squared-error ('mse') to calculate loss
+        mask: masks to mask-out samples when calculating the loss, usually being used as confidence-masking-out
+    """
+
+    assert name in ['ce', 'mse']
+    # logits_w = logits_w.detach()
+    if name == 'mse':
+        probs = torch.softmax(logits, dim=-1)
+        loss = F.mse_loss(probs, targets, reduction='none').mean(dim=1)
+    else:
+        loss = ce_loss(logits, targets, reduction='none')
+
+    if mask is not None:
+        # mask must not be boolean type
+        print("loss:",loss.shape)
+        loss = loss * mask
+
+    return loss.mean()
+
+class ConsistencyLoss(nn.Module):
+    """
+    Wrapper for consistency loss
+    """
+    def forward(self, logits, targets, name='ce', mask=None):
+        return consistency_loss(logits, targets, name, mask)
